14c26595b17dbfcbb632989455833f2c30e80670
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-06-07 14:04:02 +0200

    :tada: Initial structure and configuration

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+require('express-async-errors');
+require('./db');
+
+const { port } = require('./config/config');
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routers
+
+app.use(function (req, res, next) {
+	res.status(404).send("Sorry can't find that!");
+	next();
+});
+
+app.use(function (err, req, res, next) {
+	console.log(err);
+	const status = res.statusCode == 200 ? 500 : res.statusCode;
+	const message = res.statusCode == 200 ? 'Something broke!' : err.message;
+	res.status(status).json({
+		message,
+		errors: err,
+	});
+	next();
+});
+
+app.listen(port, () => {
+	console.info(`App Listening on port ${port}`);
+});
