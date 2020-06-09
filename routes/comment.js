179353b9e798f04c8d2cd3df046fb32376fa8d43
reem emad <reem.emad9611@yahoo.com>
2020-06-09 22:45:31 +0200

    "delete & edit comment"

--- routes/comment.js
+++ routes/comment.js
@@ -12,7 +12,8 @@ router.post("/:id", authenticationMiddleware, async (req, res) => {
   const post = await Post.findById({ _id: req.params.id });
 
   const { text } = req.body;
-  const comment = new Comment({ text, user: req.user._id });
+
+  const comment = new Comment({ text, autherKid: req.user._id });
   await comment.save();
   post.comments.push(comment._id);
   await post.save();
@@ -23,17 +24,24 @@ router.post("/:id", authenticationMiddleware, async (req, res) => {
 router.patch("/:postId/:id", async (req, res) => {
   const post = await Post.findById({ _id: req.params.postId });
   const comment = post.comments.find((comment) => comment == req.params.id);
-  // comment.text = req.body;
-  console.log(comment);
-  // await comment.save();
-  // res.status(200).json({ comment, message: "Comment edited successfully" });
+  if (!comment) return res.send("not found");
+  Object.keys(req.body).forEach((key) => {
+    comment[key] = req.body[key];
+  });
+  res.status(200).json({ comment, message: "Comment edited successfully" });
 });
 
 //DELETE COMMENT
-router.delete("/:id", async (req, res) => {
-  const deleted = await Comment.deleteOne({ _id: req.params.id });
-  if (!deleted) return res.send("no comments found");
+router.delete("/:id/:postId", async (req, res) => {
+  const post = await Post.findById({ _id: req.params.postId });
 
+  const deleted = post.comments.filter(
+    (commentId) => commentId != req.params.id
+  );
+
+  if (!deleted) return res.send("no comments found");
+  post.comments = deleted;
+  await post.save();
   res.status(200).json({ message: "Comment deleted successfully" });
 });
 
