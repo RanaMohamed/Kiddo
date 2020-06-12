e5868bee960aa8a3580e45fe3bca4ff81410a6c8
AmalAtef <AmalAtef@users.noreply.github.com>
2020-06-12 02:31:15 +0200

    approve post by supporter

--- routes/supporter.js
+++ routes/supporter.js
@@ -1,46 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { body } = require('express-validator');
+const { body } = require("express-validator");
 
-const validateRequest = require('../middlewares/validateRequest');
-const Supporter = require('../models/Supporter');
+const validateRequest = require("../middlewares/validateRequest");
+const authenticate = require("../middlewares/authentication");
+const Supporter = require("../models/Supporter");
+const Post = require("../models/Post");
 
-const { login } = require('../helpers/helper');
+const { login } = require("../helpers/helper");
 
-router.post('/register', async (req, res) => {
-	const {
-		username,
-		password,
-		email,
-		dateOfBirth,
-		experience,
-		category,
-	} = req.body;
-	const supporter = new Supporter({
-		username,
-		password,
-		email,
-		dateOfBirth,
-		experience,
-		category,
-	});
+router.post("/register", async (req, res) => {
+  const {
+    username,
+    password,
+    email,
+    dateOfBirth,
+    experience,
+    category
+  } = req.body;
+  const supporter = new Supporter({
+    username,
+    password,
+    email,
+    dateOfBirth,
+    experience,
+    category
+  });
 
-	await supporter.save();
-	const token = await supporter.generateToken();
+  await supporter.save();
+  const token = await supporter.generateToken();
 
-	res
-		.status(201)
-		.json({ message: 'Supporter registered successfully', supporter, token });
+  res
+    .status(201)
+    .json({ message: "Supporter registered successfully", supporter, token });
 });
 
 router.post(
-	'/login',
-	validateRequest([
-		body('username').exists().withMessage('Username is required'),
-		body('password').exists().withMessage('Password is required'),
-	]),
-	login(Supporter)
+  "/login",
+  validateRequest([
+    body("username")
+      .exists()
+      .withMessage("Username is required"),
+    body("password")
+      .exists()
+      .withMessage("Password is required")
+  ]),
+  login(Supporter)
 );
+
+// Approve Post
+router.post("/approvePost/:postId", authenticate, async (req, res) => {
+  const post = await Post.findById(req.params.postId);
+  if (post.isApproved === true)
+    return res.json({ message: "Post already approved" });
+
+  post.isApproved = true;
+  await post.save();
+  res.json({ message: "Post approved Successfully", post });
+});
 
 module.exports = router;
