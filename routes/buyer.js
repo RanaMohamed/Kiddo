98a44037e0cdbe93c63295acbe0083670bb48a58
AmalAtef <AmalAtef@users.noreply.github.com>
2020-06-13 02:57:25 +0200

    Buyer’s bought products

--- routes/buyer.js
+++ routes/buyer.js
@@ -1,39 +1,65 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { body } = require('express-validator');
+const { body } = require("express-validator");
 
-const validateRequest = require('../middlewares/validateRequest');
-const Buyer = require('../models/Buyer');
+const validateRequest = require("../middlewares/validateRequest");
+const authenticate = require("../middlewares/authentication");
+const authorize = require("../middlewares/authorize");
+const Buyer = require("../models/Buyer");
+const Product = require("../models/product");
 
-const { login } = require('../helpers/helper');
+const { login } = require("../helpers/helper");
 
-router.post('/register', async (req, res) => {
-	const { username, password, email, dateOfBirth, phone, address } = req.body;
-	const buyer = new Buyer({
-		username,
-		password,
-		email,
-		dateOfBirth,
-		phone,
-		address,
-	});
+router.post("/register", async (req, res) => {
+  const { username, password, email, dateOfBirth, phone, address } = req.body;
+  const buyer = new Buyer({
+    username,
+    password,
+    email,
+    dateOfBirth,
+    phone,
+    address
+  });
 
-	await buyer.save();
-	const token = await buyer.generateToken();
+  await buyer.save();
+  const token = await buyer.generateToken();
 
-	res
-		.status(201)
-		.json({ message: 'Buyer registered successfully', buyer, token });
+  res
+    .status(201)
+    .json({ message: "Buyer registered successfully", buyer, token });
 });
 
 router.post(
-	'/login',
-	validateRequest([
-		body('username').exists().withMessage('Username is required'),
-		body('password').exists().withMessage('Password is required'),
-	]),
-	login(Buyer)
+  "/login",
+  validateRequest([
+    body("username")
+      .exists()
+      .withMessage("Username is required"),
+    body("password")
+      .exists()
+      .withMessage("Password is required")
+  ]),
+  login(Buyer)
 );
+
+//get latest Buyer’s bought products
+router.get("/products", authenticate, authorize("Buyer"), async (req, res) => {
+  const totalNumOfProducts = await Product.countDocuments({
+    buyer: req.user._id
+  });
+  if (totalNumOfProducts === 0)
+    return res.status(400).json({ message: "You didn't buy any product" });
+
+  const Products = await Product.find({ buyer: req.user._id })
+    .sort({ _id: -1 })
+    .skip((parseInt(req.query.pageNum) - 1) * parseInt(req.query.size))
+    .limit(parseInt(req.query.size))
+    .populate({
+      path: "post",
+      select: "_id title body autherKid attachedFiles"
+    });
+  res.send({ Products, totalNumOfProducts });
+});
 
 module.exports = router;
