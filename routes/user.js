80e4b6c7f11f2e1aea850e57773d1041d8d90356
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-06-21 09:33:16 +0200

    :sparkles: Get Logged in user

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { body } = require("express-validator");
-const Kid = require("../models/kid");
-const Supporter = require("../models/supporter");
-const Buyer = require("../models/buyer");
+const Kid = require('../models/kid');
+const Supporter = require('../models/supporter');
+const Buyer = require('../models/buyer');
+const authenticate = require('../middlewares/authentication');
 
-router.get("/:id", async (req, res) => {
-  const user =
-    (await Kid.findById({ _id: req.params.id })) ||
-    (await Supporter.findById({ _id: req.params.id })) ||
-    (await Buyer.findById({ _id: req.params.id }));
-  res.status(200).send(user);
+router.get('/', authenticate, (req, res) => {
+	res.json({ user: req.user, type: req.userType });
+});
+
+router.get('/:id', async (req, res) => {
+	const user =
+		(await Kid.findById({ _id: req.params.id })) ||
+		(await Supporter.findById({ _id: req.params.id })) ||
+		(await Buyer.findById({ _id: req.params.id }));
+	res.status(200).send(user);
 });
 
 module.exports = router;
