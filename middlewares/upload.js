8b414339466d12a073b22f211d31c5f907f82e8c
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-06-23 15:11:24 +0200

    :alien: Upload with cloudinary

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,9 +2,9 @@ const multer = require("multer");
 const path = require("path");
 /** Storage Engine */
 const storageEngine = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, "uploads");
-	},
+	// destination: function (req, file, cb) {
+	// 	cb(null, "uploads");
+	// },
 	filename: function (req, file, cb) {
 		cb(null, Date.now() + path.extname(file.originalname));
 	},
